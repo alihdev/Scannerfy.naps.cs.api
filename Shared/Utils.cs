bc8e59a142e1bb0061c9fcf82f4f9523cc430d77
Ali Hassan <97635301+alihdev@users.noreply.github.com>
2025-02-23 00:33:19 +0300

    Add Utils IsPortInUse

---
+++ Shared/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace Scannerfy.Api.Shared;
+
+public static class Utils
+{
+    public static bool IsPortInUse(int port)
+    {
+        try
+        {
+            // Attempt to bind to the port
+            var tcpListener = new TcpListener(IPAddress.Loopback, port);
+            tcpListener.Start();
+            tcpListener.Stop();
+            return false; // Port is available
+        }
+        catch (SocketException)
+        {
+            return true; // Port is in use
+        }
+    }
+}
